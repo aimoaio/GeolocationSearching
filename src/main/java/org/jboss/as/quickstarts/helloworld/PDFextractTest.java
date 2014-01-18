43d461e4ca0bccfccba1df1f106a9a255edbf430
SeeMai <aimoaio@gmail.com>
2014-01-18 16:59:37 +0000

    test x

---
+++ src/main/java/org/jboss/as/quickstarts/helloworld/PDFextractTest.java
@@ -0,0 +1,28 @@
+package org.jboss.as.quickstarts.helloworld;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
+
+
+public class PDFextractTest {
+
+	public static void main(String[] args) {
+		PDDocument document;
+		try {
+			PDFTextStripper stripper;
+			document = PDDocument.load(new File("war\\norwegiantext.pdf"));
+			stripper = new PDFTextStripper();
+			stripper.setSortByPosition( true );
+			System.out.println(stripper.getText(document));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
