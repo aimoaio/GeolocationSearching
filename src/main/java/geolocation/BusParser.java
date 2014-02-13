8cec6aa3ebaa01589bb7a9ce4412caf969cd0d79
SeeMai <aimoaio@gmail.com>
2014-02-13 22:33:04 +0000

    Added more if/else checks to figure out how to format rows properly. Rows with numbers and no dashes will have separate <td> tags per token excluding the first instances with no numbers.
    Dashes will work the same way as numbers in terms of <td> tagging. Code
    is almost exactly the same.
    
    Added spacing for easier reading. Mainly for place names in which
    appended string was all joined together.
    
    Still need a check for the Valid to/from date in which is currently in a
    table cell itself when it shouldn't be. This warps the format slightly
    but isn't a major problem. Should still be fixed though.

--- src/main/java/geolocation/BusParser.java
+++ src/main/java/geolocation/BusParser.java
@@ -21,17 +21,90 @@ public class BusParser {
 		colours.add("pink");
 		colours.add("red");
 		
+//		for(String row:contents){
+//			ArrayList<String> cells = splitStringSpace(row);
+//			for(String cell: cells){
+//				StringBuilder builder = new StringBuilder();
+//				builder.append("<td>");
+//				builder.append(cell);
+//				builder.append("</td>");
+//				finalCellList.add(builder.toString());
+//			}
+//		}
+//		
+		int position = 0;
+		int dashposition = 0;
 		for(String row:contents){
-			ArrayList<String> cells = splitStringSpace(row);
-			for(String cell: cells){
-				StringBuilder builder = new StringBuilder();
-				builder.append("<td>");
-				builder.append(cell);
-				builder.append("</td>");
-				finalCellList.add(builder.toString());
+			System.out.println("Row: " + row);
+			
+			if(numberOfDigits(row)>2 && containsDash(row)==false){
+				System.out.println("Digits: " + numberOfDigits(row));
+				ArrayList<String> cells = splitStringSpace(row);
+				position = firstPositionOfNumbers(cells);
+				
+				StringBuilder b1 = new StringBuilder();
+				
+				for (int j=0; j<position;j++){
+					b1.append(cells.get(j));
+					b1.append(" ");
+				}
+				b1.append(" </td>");
+				b1.insert(4, " <td>");
+				finalCellList.add(b1.toString());
+				System.out.println("E Cells: " + b1.toString());
+				
+				for(int i=position;i<cells.size(); i++){
+					StringBuilder b = new StringBuilder();
+					b.append("<td>");
+					b.append(cells.get(i));
+					//int CL = cells.get(i).length();
+					//b.insert(CL-1, "</td>");
+					b.append(" ");
+					b.append("</td>");
+					finalCellList.add(b.toString());
+					System.out.println("Cells: " + b.toString());
+				}
+			}  else if(containsDash(row)){
+				
+				System.out.println("Dash found");
+				ArrayList<String> cells = splitStringSpace(row);
+				dashposition = firstPositionOfDashes(cells);
+				
+				StringBuilder b1 = new StringBuilder();
+				
+				for (int j=0; j<position;j++){
+					b1.append(cells.get(j));
+				}
+				b1.append("</td>");
+				b1.insert(4, "<td>");
+				finalCellList.add(b1.toString());
+				System.out.println("D Cells: " + b1.toString());
+				
+				for(int i=position;i<cells.size(); i++){
+					StringBuilder b = new StringBuilder();
+					b.append("<td>");
+					b.append(cells.get(i));
+					b.append("</td>");
+					finalCellList.add(b.toString());
+					System.out.println("D2 Cells: " + b.toString());
+				}
+			}
+			else if(row.equals("<table>") || row.equals("</table>")){
+			
+				
+			finalCellList.add(row);
+			
+			}
+				else {
+				StringBuilder sb = new StringBuilder();
+				sb.append(row);
+				sb.insert(4, "<td>");
+				sb.insert(row.length()-2, "</td>");
+				System.out.println("Row length: " + row.length());
+				finalCellList.add(sb.toString());
+				System.out.println("Unedited: " + sb.toString());
 			}
 		}
-		
 		StringBuilder b = new StringBuilder();
 		for(String s:finalCellList){
 			b.append(s);
@@ -47,7 +120,7 @@ public class BusParser {
 			//System.out.println("Terms: " + replace);
 			String colour = colours.get(j);
 			String newterm = "<span id=\""+replace+"\" style='background-color:"+ colour + ";'>" + replace + "</span>";
-			System.out.println("new line is: " + newterm);
+			//System.out.println("new line is: " + newterm);
 			s = s.replace(replace, newterm);
 			
 			}
@@ -58,11 +131,73 @@ public class BusParser {
 		return finalcontents;
 	}
 	
+
 	private static ArrayList<String> splitStringSpace(String str){
 		ArrayList<String> split = new ArrayList<String>();
 		String[] splited = str.split("\\s+");
 		Collections.addAll(split, splited); 
 		return split;
 	}
+	public final static boolean containsDigit(String s) {
+	    boolean containsDigit = false;
+
+	    if (s != null && !s.isEmpty()) {
+	        for (char c : s.toCharArray()) {
+	            if (containsDigit = Character.isDigit(c)) {
+	                break;
+	            }
+	        }
+	    }
+
+	    return containsDigit;
+	}
+	
+	public final static boolean containsDash(String s) {
+	    boolean containsDash = false;
+
+	    if (s != null && !s.isEmpty()) {
+	        if(s.contains("----")){
+	        	containsDash = true;
+	        }
+	    }
+
+	    return containsDash;
+	}
+	
+	public final static int numberOfDigits(String s) {
+	    int counter=0;
+
+	    if (s != null && !s.isEmpty()) {
+	        for (char c : s.toCharArray()) {
+	            if (Character.isDigit(c)) {
+	            	counter++;
+	            }
+	        }
+	    }
+
+	    return counter;
+	}
+		
+		private static int firstPositionOfNumbers(ArrayList<String> s){
+			int position = 0;
+			for(int i=0;i<s.size();i++){
+				if(containsDigit(s.get(i))){
+					position = i;
+					break;
+				}
+			} return position;
+
+		}
+		
+		private static int firstPositionOfDashes(ArrayList<String> s){
+			int position = 0;
+			for(int i=0;i<s.size();i++){
+				if(s.get(i).contains("-")){
+					position = i;
+					break;
+				}
+			} return position;
+
+		}
 
 }
--- src/main/java/geolocation/PdfBoxGAEDemo.java
+++ src/main/java/geolocation/PdfBoxGAEDemo.java
@@ -12,6 +12,8 @@ package geolocation;
 
 
 
+
+
 import java.awt.geom.Rectangle2D;
 import java.awt.geom.Rectangle2D.Double;
 import java.net.HttpURLConnection;
@@ -29,219 +31,206 @@ import org.apache.pdfbox.util.PDFTextStripperByArea;
 
 public class PdfBoxGAEDemo {
 
+	private static final Logger log = Logger.getLogger(PdfBoxGAEDemo.class.getName());
+	public static final String NEW_LINE = System.getProperty("line.separator");
+	
+	
+	static ArrayList<String> text;
+	static String finaltext;
+	static String s;
+	static int linecounter;
+	static ArrayList<String> tokens;
+	static ArrayList<String> cells;
+	static ArrayList<String> HTML;
+	static ArrayList<String> HTMLFINAL;
 
-private static final Logger log = Logger.getLogger(PdfBoxGAEDemo.class.getName());
-public static final String NEW_LINE = System.getProperty("line.separator");
-
-
-static ArrayList<String> text;
-static String finaltext;
-static String s;
-static int linecounter;
-static ArrayList<String> tokens;
-static ArrayList<String> cells;
-static ArrayList<String> HTML;
-static ArrayList<String> HTMLFINAL;
-
-
+	
 	public static ArrayList<String> Exec(String pdfUrl, int x, int y, int w, int h, ArrayList<String> terms) {
 
-	log.info("PdfUrl=" + pdfUrl);
-	System.out.println("didnt get into try");
-	text = new ArrayList<String>();
-	tokens = new ArrayList<String>();
-	cells = new ArrayList<String>();
-	HTML = new ArrayList<String>();
-	HTMLFINAL = new ArrayList<String>();
-	
-	
-	ArrayList<String> searchterms = terms;
-	linecounter =0;
-
-	try {
-		URL urlObj = new URL(pdfUrl);
-		HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
-		connection.addRequestProperty("Cache-Control", "no-cache,max-age=0");
-		connection.addRequestProperty("Pragma", "no-cache");
-		connection.setInstanceFollowRedirects(false);
-		int httpRespCode = connection.getResponseCode();
-	
-
-		if (httpRespCode == 200) {
-			RandomAccessBuffer tempMemBuffer = new RandomAccessBuffer();
-			PDDocument doc = PDDocument.load(connection.getInputStream(), tempMemBuffer);
-			
-
-			PDFTextStripperByArea sa = new PDFTextStripperByArea();
-
-			
-			sa.addRegion("Area1", new Rectangle2D.Double(x, y, w, h));
+		log.info("PdfUrl=" + pdfUrl);
+		System.out.println("didnt get into try");
+		text = new ArrayList<String>();
+		tokens = new ArrayList<String>();
+		cells = new ArrayList<String>();
+		HTML = new ArrayList<String>();
+		HTMLFINAL = new ArrayList<String>();
 		
-			//PDPage p = (PDPage) doc.getDocumentCatalog().getAllPages().get(0); //this line is the problem
-			PDDocumentCatalog cat = doc.getDocumentCatalog();
+		
+		ArrayList<String> searchterms = terms;
+		linecounter =0;
 	
-			//System.out.println(cat.toString());
-			List <PDPage> pages = cat.getAllPages();
-			//
+		try {
+			URL urlObj = new URL(pdfUrl);
+			HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
+			connection.addRequestProperty("Cache-Control", "no-cache,max-age=0");
+			connection.addRequestProperty("Pragma", "no-cache");
+			connection.setInstanceFollowRedirects(false);
+			int httpRespCode = connection.getResponseCode();
+		
+
+			if (httpRespCode == 200) {
+				RandomAccessBuffer tempMemBuffer = new RandomAccessBuffer();
+				PDDocument doc = PDDocument.load(connection.getInputStream(), tempMemBuffer);
+				
+
+				PDFTextStripperByArea sa = new PDFTextStripperByArea();
+	
+				
+				sa.addRegion("Area1", new Rectangle2D.Double(x, y, w, h));
 			
-			//loop for pages
-			for (int i=0;i<2; i++){
-				PDPage p = pages.get(i);
-				sa.extractRegions(p);
+				//PDPage p = (PDPage) doc.getDocumentCatalog().getAllPages().get(0); //this line is the problem
+				PDDocumentCatalog cat = doc.getDocumentCatalog();
+		
+				//System.out.println(cat.toString());
+				List <PDPage> pages = cat.getAllPages();
+				//
 				
-				//s is a string of the PAGE
-				s = sa.getTextForRegion("Area1"); //get the text for the page
-				
-				
-				
+				//loop for pages
+				for (int i=0;i<2; i++){
+					PDPage p = pages.get(i);
+					sa.extractRegions(p);
 					
-					linecounter = countLines(s);
+					//s is a string of the PAGE
+					s = sa.getTextForRegion("Area1"); //get the text for the page
 					
-					//adding tr to all new lines
-					tokens = splitStrings(s); //split by line
-					for(String tk:tokens){
-					
-						//for every row TK, append relevant tags
-						StringBuilder sb = new StringBuilder();
-						sb.append("<tr>");
-						sb.append(tk);
-						sb.append("</tr> ");
-						System.out.println(sb.toString());
+						linecounter = countLines(s);
 						
-						String row = sb.toString(); //row of text with tags
-						HTML.add(row); //add row of text with tags
-					}
-					
-					//compile previous tags to string first
-					StringBuilder b = new StringBuilder();
-					for(String s:HTML){
-						b.append(s);
-					}
+						//adding tr to all new lines
+						tokens = splitStrings(s); //split by line
+						for(String tk:tokens){
+						
+							//for every row TK, append relevant tags
+							StringBuilder sb = new StringBuilder();
+							sb.append("<tr>");
+							sb.append(tk);
+							sb.append("</tr> ");
+							//System.out.println(sb.toString());
+							
+							String row = sb.toString(); //row of text with tags
+							HTML.add(row); //add row of text with tags
+						}
+						
+						//compile previous tags to string first
+//						StringBuilder b = new StringBuilder();
+//						for(String s:HTML){
+//							b.append(s);
+//						}
+				}
+				
+				//text per page
+				text.add("<table>");
+				for(String t:HTML){
+				text.add(t);
+				}
+				text.add("</table>");
+				text.add("<br/>");
+				doc.close();
+				
+
+			} else{
+				System.out.println("didnt get into if");
+				throw new Exception("Http return code <> 200. Received: " + httpRespCode);
 			}
-			
-			//text per page
-			text.add("<table>");
-			StringBuilder stringer = new StringBuilder();
-			for(String t:HTML){
-				stringer.append(t);
-			}
-			text.add(stringer.toString());
-			text.add("</table>");
-			text.add("<br/>");
-			//System.out.println("T: " + text.toString());
-			StringBuilder stringbuild = new StringBuilder();
-			for(String t:text){
-				stringbuild.append(t);
-			}
-			finaltext = stringbuild.toString();
-			
-			doc.close();
 			
 
-		} else{
-			System.out.println("didnt get into if");
-			throw new Exception("Http return code <> 200. Received: " + httpRespCode);
+		} catch (Exception e) {
+			log.severe("EXCEPTION: " + e.toString());
+			//return "*** EXCEPTION *** " + e.toString();
+		}
+		return text;
+	}
+	
+	//for no numbers
+	private static String formatMethod2(String row) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	//for numbers
+	private static String formatMethod1(String row) {
+		ArrayList<String> finalstringcontents = new ArrayList<String>();
+		ArrayList<String> celldata = new ArrayList<String>();
+		
+		//split row into cells
+		celldata = splitStringSpace(row); 
+		
+		//find first occurence of numbers
+		int position = firstPositionOfNumbers(celldata); 	
+		
+		//for everything before that point we append into 1 string
+		
+		StringBuilder compile = new StringBuilder();
+		compile.append("<td>");
+		for(int f=0; f<position;f++){
+			
+			compile.append(celldata.get(f));
+		}
+		compile.append("</td>");
+		
+		//add to final list
+		finalstringcontents.add(compile.toString());
+		
+		//for everything that point onwards we append separate td tags for each cell
+		for(int e=position;e<celldata.size();e++){
+			StringBuilder build1 = new StringBuilder();
+			build1.append("<td>");
+			build1.append(celldata.get(e));
+			build1.append("</td>");
+			
+			//add our newbuild string to list.
+			finalstringcontents.add(build1.toString());
 		}
 		
-
-	} catch (Exception e) {
-		log.severe("EXCEPTION: " + e.toString());
-		//return "*** EXCEPTION *** " + e.toString();
+		String finalstring = finalstringcontents.toString();
+		System.out.println("F string: " + finalstring);
+		return finalstring;
 	}
-	return text;
-}
 
-//for no numbers
-private static String formatMethod2(String row) {
-	// TODO Auto-generated method stub
-	return null;
-}
-
-
-//for numbers
-private static String formatMethod1(String row) {
-	ArrayList<String> finalstringcontents = new ArrayList<String>();
-	ArrayList<String> celldata = new ArrayList<String>();
+	private static int countLines(String str){
+		   String[] lines = str.split("\r\n|\r|\n");
+		   return  lines.length;
+		}
 	
-	//split row into cells
-	celldata = splitStringSpace(row); 
-	
-	//find first occurence of numbers
-	int position = firstPositionOfNumbers(celldata); 	
-	
-	//for everything before that point we append into 1 string
-	
-	StringBuilder compile = new StringBuilder();
-	compile.append("<td>");
-	for(int f=0; f<position;f++){
+	private static ArrayList<String> splitStrings(String str){
+		ArrayList<String> split = new ArrayList<String>();
+		String[] lines = str.split("\r\n|\r|\n");
+		Collections.addAll(split, lines); 
 		
-		compile.append(celldata.get(f));
+		return split;
 	}
-	compile.append("</td>");
 	
-	//add to final list
-	finalstringcontents.add(compile.toString());
+	private static ArrayList<String> splitStringSpace(String str){
+		ArrayList<String> split = new ArrayList<String>();
+		String[] splited = str.split("\\s+");
+		Collections.addAll(split, splited); 
+		return split;
+	}
 	
-	//for everything that point onwards we append separate td tags for each cell
-	for(int e=position;e<celldata.size();e++){
-		StringBuilder build1 = new StringBuilder();
-		build1.append("<td>");
-		build1.append(celldata.get(e));
-		build1.append("</td>");
+
+	public final static boolean containsDigit(String s) {
+	    boolean containsDigit = false;
+
+	    if (s != null && !s.isEmpty()) {
+	        for (char c : s.toCharArray()) {
+	            if (containsDigit = Character.isDigit(c)) {
+	                break;
+	            }
+	        }
+	    }
+
+	    return containsDigit;
+	}
 		
-		//add our newbuild string to list.
-		finalstringcontents.add(build1.toString());
-	}
-	
-	String finalstring = finalstringcontents.toString();
-	System.out.println("F string: " + finalstring);
-	return finalstring;
-}
+		private static int firstPositionOfNumbers(ArrayList<String> s){
+			int position = 0;
+			for(int i=0;i<s.size();i++){
+				if(containsDigit(s.get(i))){
+					position = i;
+					break;
+				}
+			} return position;
 
-private static int countLines(String str){
-	   String[] lines = str.split("\r\n|\r|\n");
-	   return  lines.length;
-	}
-
-private static ArrayList<String> splitStrings(String str){
-	ArrayList<String> split = new ArrayList<String>();
-	String[] lines = str.split("\r\n|\r|\n");
-	Collections.addAll(split, lines); 
-	
-	return split;
-}
-
-private static ArrayList<String> splitStringSpace(String str){
-	ArrayList<String> split = new ArrayList<String>();
-	String[] splited = str.split("\\s+");
-	Collections.addAll(split, splited); 
-	return split;
-}
-
-
-public final static boolean containsDigit(String s) {
-    boolean containsDigit = false;
-
-    if (s != null && !s.isEmpty()) {
-        for (char c : s.toCharArray()) {
-            if (containsDigit = Character.isDigit(c)) {
-                break;
-            }
-        }
-    }
-
-    return containsDigit;
-}
-	
-	private static int firstPositionOfNumbers(ArrayList<String> s){
-		int position = 0;
-		for(int i=0;i<s.size();i++){
-			if(containsDigit(s.get(i))){
-				position = i;
-				break;
-			}
-		} return position;
-
-	}
+		}
 
 }
