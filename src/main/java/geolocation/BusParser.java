a35d96d6fa556397d7f86e6f46c611768207b8e8
SeeMai <aimoaio@gmail.com>
2014-02-23 18:55:56 +0000

    This update contains significant code tidying. -Also contains documentation -Commented areas of code for better explanation of how they work. -Removed unused variables and methods -Brackets and If/Else, Loop statements are now indented correctly.
    Everything should be much easier to understand and more legible.

--- src/main/java/geolocation/BusParser.java
+++ src/main/java/geolocation/BusParser.java
@@ -1,5 +1,15 @@
 package geolocation;
 
+/** 
+ * This class is for parsing the FirstGroup bus timetables. This means only
+ * FirstBus PDF timetables will be formatted correctly as such. Other PDFs
+ * will fail to display correctly due to the detection systems in place designed
+ * for tables and row information.
+ * Therefore it is recommended that a new check for the type of PDF document
+ * be implemented in the demo class to ensure that only timetables from the firstgroup
+ * website are passed into this class when deciding on the parser for formatting.
+ */
+
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -9,13 +19,13 @@ public class BusParser {
 	
 	public static String Exec(ArrayList<String> contents, ArrayList<String> geoterms) {
 		
-		ArrayList<String> finalList = new ArrayList<String>();
+
 		ArrayList<String> finalCellList = new ArrayList<String>();
 		ArrayList<String> colours = new ArrayList<String>();
 		
 		colours = new ArrayList<String>();
 		
-		//add colours
+		//add colour variation for highlighting terms
 		colours.add("blue");
 		colours.add("yellow");
 		colours.add("green");
@@ -23,21 +33,22 @@ public class BusParser {
 		colours.add("pink");
 		colours.add("red");
 		
-//		for(String row:contents){
-//			ArrayList<String> cells = splitStringSpace(row);
-//			for(String cell: cells){
-//				StringBuilder builder = new StringBuilder();
-//				builder.append("<td>");
-//				builder.append(cell);
-//				builder.append("</td>");
-//				finalCellList.add(builder.toString());
-//			}
-//		}
-//		
+		//set initial positions of cell contents to 0
 		int position = 0;
 		int dashposition = 0;
+		
+		
+		//looping over every row of contents extracted previously
 		for(String row:contents){
 			System.out.println("Row: " + row);
+			
+			
+			/* If the row contains the world valid, then it is a date and despite containing
+			 * over 4 digits (time data), it should not be placed in a different table cell
+			 * as this would create a massive space between the first two rows resulting in
+			 * warped formatting. As such, it is checked first before any other number check
+			 * is implemented.
+			 */
 			
 			if(row.contains("Valid")){
 				StringBuilder sb = new StringBuilder();
@@ -48,6 +59,19 @@ public class BusParser {
 				finalCellList.add(sb.toString());
 				System.out.println("Valid row: " + sb.toString());
 			}
+			
+			/*
+			 * If the row contains more than 2 digits (meaning it is not just the bus service
+			 * number alone, and does not contain any dashes (meaning no available time), then
+			 * format using this method. This will check for the first position in which time data
+			 * is detected (referred to as "position").
+			 * Any cells found before the position mark, is therefore a place name and should be
+			 * appended into 1 table cell with spaces between each cell. The <td> initiating tag
+			 * should be inserted at the start of the name, and after the <tr> tags (therefore the 
+			 * position for insertion is at 4 because <tr> is 4 in length).
+			 * Any cells found after the position mark, is time data and should be in a cell of its own,
+			 * these are just appended with <td> tags.
+			 */
 			
 			else if(numberOfDigits(row)>2 && containsDash(row)==false){
 				System.out.println("Digits: " + numberOfDigits(row));
@@ -74,16 +98,35 @@ public class BusParser {
 					finalCellList.add(b.toString());
 					System.out.println("Cells: " + b.toString());
 				}
-			}  else if(numberOfDigits(row)>2 && containsDash(row)==true){
+			}  
+			
+			/*
+			 * If the row contains more than 2 digits and also contains dashes
+			 * then we need to find out if the first instance is a time cell or a
+			 * dash cell. If it's a time cell that contains time data, then simply
+			 * apply the <td> tags to the first cell (at position 4, after <tr>) and also
+			 * to every cell from that point onwards.
+			 * this requires an extra <td> tag to be added because the row starts with a
+			 * time data cell and not with a place name. this tends to happen when a place
+			 * name is too long and thus written over two lines. The PDF parser will detect
+			 * this on separate lines and will then set the time cells on a different row itself,
+			 * using the previous formatting plans will skew up the time cells to the left
+			 * instead of aligning to the right.
+			 * 
+			 * if the first cell instance is not a time data cell, we must look and check for the
+			 * first position of number and dash occurrence. the positions are then compared and 
+			 * the lower number is set as the loop number for the rest of the cells. Anything before
+			 * this position, will require the <td> tag inserted at position 4 because of the <tr> tags
+			 * and then everything within that area and the positon is appended.
+			 * Everything afterwards can be looped and appended appropriately. 
+			 */
+			
+			else if(numberOfDigits(row)>2 && containsDash(row)==true){
 				System.out.println("Digits: " + numberOfDigits(row));
 				ArrayList<String> cells = splitStringSpace(row);
 				
 				if(isTimeValue(cells.get(0))==true){
-					
-					//loop around everything and apply extra td
-					//first one is special
 					StringBuilder sb = new StringBuilder();
-					
 					sb.append(cells.get(0));
 					sb.insert(4, "<td>");
 					sb.append("</td>");
@@ -102,24 +145,28 @@ public class BusParser {
 						System.out.println("Special Cells: " + b.toString());
 					}
 					
-				} else {
+				} 
+				
+				else {
 				
 				position = firstPositionOfNumbers(cells);
 				dashposition = firstPositionOfDashes(cells);
-				
 				int finalposition = 0;
-				if(position>dashposition){
-					finalposition = dashposition;
-				} else {
-					finalposition = position;
-				}
+				
+					if(position>dashposition){
+						finalposition = dashposition;
+					} 
+					
+					else {
+						finalposition = position;
+					}
 				
 				StringBuilder b1 = new StringBuilder();
-				
 				for (int j=0; j<finalposition;j++){
 					b1.append(cells.get(j));
 					b1.append(" ");
 				}
+				
 				b1.append("</td>");
 				b1.insert(4, "<td>");
 				finalCellList.add(b1.toString());
@@ -136,19 +183,27 @@ public class BusParser {
 				}
 				}
 			
-			} else if(containsDash(row)){
+				} 
 			
-				
+			/*
+			 * If the row fails the above checks but contains dashes, then it simply
+			 * means it is missing time information (unavailable or no bus). In which 
+			 * case, we simply append contents similarly to numbers previously. 
+			 * First occurrence of dash = position. Anything before is appended
+			 * into its own <td> tag (likely to be a placename), and everything after
+			 * is appended into separate tags.
+			 */
+				else if(containsDash(row)){
 				System.out.println("Dash found");
 				ArrayList<String> cells = splitStringSpace(row);
 				dashposition = firstPositionOfDashes(cells);
 				
 				StringBuilder b1 = new StringBuilder();
-				
 				for (int j=0; j<dashposition;j++){
 					b1.append(cells.get(j));
 					b1.append(" ");
 				}
+				
 				b1.append("</td>");
 				b1.insert(4, "<td>");
 				finalCellList.add(b1.toString());
@@ -163,14 +218,29 @@ public class BusParser {
 					finalCellList.add(b.toString());
 					System.out.println("D2 Cells: " + b.toString());
 				}
-			}
-			else if(row.equals("<table>") || row.equals("</table>")){
+				}
 			
-				
-			finalCellList.add(row);
+				/*
+				 * If the row does not contain anything else but contains
+				 * the tags <table> then simply add the original row contents
+				 * to the list. No <td> tags need to be added as it is the end
+				 * of the contents.
+				 */
 			
-			}
+				else if(row.equals("<table>") || row.equals("</table>")){
+					finalCellList.add(row);
+				}
+			
+			
+				/*
+				 * Else the row should be unedited. With the exception of adding in
+				 * <td> tags so they are placed in a row on their own in the table.
+				 * This is for additional information such as the service numbers or notes or
+				 * any other text that is not timetable information and thus, should not
+				 * be aligned into columns and rows.
+				 */
 				else {
+					
 				StringBuilder sb = new StringBuilder();
 				sb.append(row);
 				sb.insert(4, "<td>");
@@ -178,8 +248,11 @@ public class BusParser {
 				System.out.println("Row length: " + row.length());
 				finalCellList.add(sb.toString());
 				System.out.println("Unedited: " + sb.toString());
-			}
+				}
+		
 		}
+		
+		//the newly formatted cells are then appended into a single string
 		StringBuilder b = new StringBuilder();
 		for(String s:finalCellList){
 			b.append(s);
@@ -187,20 +260,28 @@ public class BusParser {
 		
 		String s = b.toString();
 		
-		//add the highlighting
+		//and highlighting is added to the string
 		if(geoterms.size()!=0){
 			
+			
+			/*
+			 * This will take every term inside the geoterm list (the list of terms to search for)
+			 * For each term, it will assign the colour in the same position from the colour list
+			 * (this means each term will have a different colour to distinguish them from the other terms)
+			 * The system will then replace the term with a newly created string based on the geoterm,
+			 * which will consist of: the span id tag (for jump links), the term and the unique highlight colour.
+			 */
 			for(int j=0;j<geoterms.size();j++){
 			String replace = geoterms.get(j);
-			//System.out.println("Terms: " + replace);
 			String colour = colours.get(j);
 			String newterm = "<span id=\""+replace+"\" style='background-color:"+ colour + ";'>" + replace + "</span>";
-			//System.out.println("new line is: " + newterm);
-			s = s.replace(replace, newterm);
+				s = s.replace(replace, newterm);
 			
 			}
 		}
 		
+		
+		//the final string to be returned for display is the string that was replaced with highlighting above
 		String finalcontents = s;
 		
 		return finalcontents;
@@ -213,6 +294,7 @@ public class BusParser {
 		Collections.addAll(split, splited); 
 		return split;
 	}
+	
 	public final static boolean containsDigit(String s) {
 	    boolean containsDigit = false;
 
--- src/main/java/geolocation/PdfBoxGAEDemo.java
+++ src/main/java/geolocation/PdfBoxGAEDemo.java
@@ -1,21 +1,18 @@
 package geolocation;
 
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * This class strips text from the PDF document and appends <tr> tags to each new line
+ * of text extracted. Line detection is done checking for new lines \n or carriage returns \r
+ * each line is then separated from the original string of text and tags are added.
+ * To ensure lines are in some sort of vague format, <table> tags are added for current 
+ * formatting. In later versions, it's possible that these will be removed and be for the BusParser 
+ * only with this class being the "if all else fails to match" class for simple text extraction 
+ * and display with zero formatting.
+ */
 
 
 
 import java.awt.geom.Rectangle2D;
-import java.awt.geom.Rectangle2D.Double;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -29,35 +26,28 @@ import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.util.PDFTextStripperByArea;
 
+
+
+
 public class PdfBoxGAEDemo {
 
 	private static final Logger log = Logger.getLogger(PdfBoxGAEDemo.class.getName());
-	public static final String NEW_LINE = System.getProperty("line.separator");
 	
 	
 	static ArrayList<String> text;
-	static String finaltext;
 	static String s;
-	static int linecounter;
 	static ArrayList<String> tokens;
-	static ArrayList<String> cells;
 	static ArrayList<String> HTML;
-	static ArrayList<String> HTMLFINAL;
 
 	
 	public static ArrayList<String> Exec(String pdfUrl, int x, int y, int w, int h, ArrayList<String> terms) {
 
 		log.info("PdfUrl=" + pdfUrl);
-		System.out.println("didnt get into try");
 		text = new ArrayList<String>();
 		tokens = new ArrayList<String>();
-		cells = new ArrayList<String>();
 		HTML = new ArrayList<String>();
-		HTMLFINAL = new ArrayList<String>();
 		
-		
-		ArrayList<String> searchterms = terms;
-		linecounter =0;
+	
 	
 		try {
 			URL urlObj = new URL(pdfUrl);
@@ -74,16 +64,10 @@ public class PdfBoxGAEDemo {
 				
 
 				PDFTextStripperByArea sa = new PDFTextStripperByArea();
-	
-				
 				sa.addRegion("Area1", new Rectangle2D.Double(x, y, w, h));
-			
-				//PDPage p = (PDPage) doc.getDocumentCatalog().getAllPages().get(0); //this line is the problem
 				PDDocumentCatalog cat = doc.getDocumentCatalog();
-		
-				//System.out.println(cat.toString());
 				List <PDPage> pages = cat.getAllPages();
-				//
+				
 				
 				//loop for pages
 				for (int i=0;i<pages.size(); i++){
@@ -93,9 +77,7 @@ public class PdfBoxGAEDemo {
 					//s is a string of the PAGE
 					s = sa.getTextForRegion("Area1"); //get the text for the page
 					
-						linecounter = countLines(s);
 						
-						//adding tr to all new lines
 						tokens = splitStrings(s); //split by line
 						for(String tk:tokens){
 						
@@ -104,21 +86,15 @@ public class PdfBoxGAEDemo {
 							sb.append("<tr>");
 							sb.append(tk);
 							sb.append("</tr> ");
-							//System.out.println(sb.toString());
 							
 							String row = sb.toString(); //row of text with tags
 							HTML.add(row); //add row of text with tags
 						}
 						
-						//compile previous tags to string first
-//						StringBuilder b = new StringBuilder();
-//						for(String s:HTML){
-//							b.append(s);
-//						}
 				}
 				
 				//text per page
-				text.add("<table>");
+				text.add("<table>"); //placing table tags means all the data will be formatted as such.
 				for(String t:HTML){
 				text.add(t);
 				}
@@ -127,69 +103,16 @@ public class PdfBoxGAEDemo {
 				
 
 			} else{
-				System.out.println("didnt get into if");
 				throw new Exception("Http return code <> 200. Received: " + httpRespCode);
 			}
 			
 
 		} catch (Exception e) {
 			log.severe("EXCEPTION: " + e.toString());
-			//return "*** EXCEPTION *** " + e.toString();
 		}
 		return text;
 	}
 	
-	//for no numbers
-	private static String formatMethod2(String row) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
-	//for numbers
-	private static String formatMethod1(String row) {
-		ArrayList<String> finalstringcontents = new ArrayList<String>();
-		ArrayList<String> celldata = new ArrayList<String>();
-		
-		//split row into cells
-		celldata = splitStringSpace(row); 
-		
-		//find first occurence of numbers
-		int position = firstPositionOfNumbers(celldata); 	
-		
-		//for everything before that point we append into 1 string
-		
-		StringBuilder compile = new StringBuilder();
-		compile.append("<td>");
-		for(int f=0; f<position;f++){
-			
-			compile.append(celldata.get(f));
-		}
-		compile.append("</td>");
-		
-		//add to final list
-		finalstringcontents.add(compile.toString());
-		
-		//for everything that point onwards we append separate td tags for each cell
-		for(int e=position;e<celldata.size();e++){
-			StringBuilder build1 = new StringBuilder();
-			build1.append("<td>");
-			build1.append(celldata.get(e));
-			build1.append("</td>");
-			
-			//add our newbuild string to list.
-			finalstringcontents.add(build1.toString());
-		}
-		
-		String finalstring = finalstringcontents.toString();
-		System.out.println("F string: " + finalstring);
-		return finalstring;
-	}
-
-	private static int countLines(String str){
-		   String[] lines = str.split("\r\n|\r|\n");
-		   return  lines.length;
-		}
 	
 	private static ArrayList<String> splitStrings(String str){
 		ArrayList<String> split = new ArrayList<String>();
@@ -199,12 +122,7 @@ public class PdfBoxGAEDemo {
 		return split;
 	}
 	
-	private static ArrayList<String> splitStringSpace(String str){
-		ArrayList<String> split = new ArrayList<String>();
-		String[] splited = str.split("\\s+");
-		Collections.addAll(split, splited); 
-		return split;
-	}
+	
 	
 
 	public final static boolean containsDigit(String s) {
@@ -221,15 +139,6 @@ public class PdfBoxGAEDemo {
 	    return containsDigit;
 	}
 		
-		private static int firstPositionOfNumbers(ArrayList<String> s){
-			int position = 0;
-			for(int i=0;i<s.size();i++){
-				if(containsDigit(s.get(i))){
-					position = i;
-					break;
-				}
-			} return position;
-
-		}
+	
 
 }
